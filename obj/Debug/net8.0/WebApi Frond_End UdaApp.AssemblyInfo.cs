f1e79857a86bf08566b099151543389be4d664c3
ELPIR0B0 <lenylopez87@gmail.com>
2024-10-29 23:25:28 -0500

    Conexion Api en inicio y registro

--- obj/Debug/net8.0/WebApi Frond_End UdaApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApi Frond_End UdaApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi Frond_End UdaApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f67453010d498a2a958202d88a463ce79da50f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi Frond_End UdaApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi Frond_End UdaApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
